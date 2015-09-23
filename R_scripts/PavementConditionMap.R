f0308cdb2e2472ebd2279e792d1bc23cbe450e76
DanielHadley <dphnrome@yahoo.com>
2015-09-23 13:18:52 -0400

    map working now to style

--- R_scripts/PavementConditionMap.R
+++ R_scripts/PavementConditionMap.R
@@ -1,5 +1,6 @@
 # Created By Daniel Hadley Tue Sep 22 14:28:25 EDT 2015 #
 setwd("/Users/DHadley/Github/SomervilleSystems/R_scripts")
+setwd("C:/Users/dhadley/Documents/GitHub/SomervilleSystems/R_scripts")
 #
 
 # Maping tools
@@ -7,6 +8,10 @@ require("rgdal") # requires sp, will use proj.4 if installed
 library(leafletR)
 
 # Load Data
-d <- readOGR(dsn="../../PCI_Code/2015_Shapefile/", layer="BL_SEGMENTS_2015")
+d <- readOGR(dsn="../../PCI_Code/2015_Shapefile", layer="BL_SEGMENTS_2015")
+
+# Was having trouble with some characters from other columns. These two seem to be fine
+d <- d[,c("OCI", "STREETNAME")]
 
 toGeoJSON(data = d, name = "PavementConditionMap", "../geo/")
+
