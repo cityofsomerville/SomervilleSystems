143dd5fa2a3e4b6632cc0413088706153e2525b0
Daniel Hadley <dphnrome@gmail.com>
2015-10-22 19:25:44 -0400

    alias update

--- R_scripts/Collisionmap.R
+++ R_scripts/Collisionmap.R
@@ -1,11 +1,13 @@
 # Created By Daniel Hadley Mon Sep 28 11:24:04 EDT 2015 #
 setwd("/Users/DHadley/Github/SomervilleSystems/R_scripts")
+setwd("//fileshare1/Departments2/Somerstat Data/SomervilleSystems/R_scripts/")
 library(readxl)
 library(dplyr)
 
 #
 # Load Data
-d <- read_excel("/Users/DHadley/P/Somerstat Data/MV_Crash_Data/data/Somerville Crash Data 2010-2015.xlsx")
+d <- read_excel("/Users/DHadley/P/Somerstat Data/MV_Crash_Data/data/")
+d <- read_excel("../../MV_Crash_Data/data/Somerville Crash Data 2010-2015 Socrata.xlsx")
 
 d <- d[,1:15]
 
